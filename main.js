c7565006d18686e542a82f80717825a9577963b2
black-mirror-1 <35616070+black-mirror-1@users.noreply.github.com>
2020-06-04 11:31:13 -0700

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const server = http.createServer((_, res) => {
   <head> \
       <title>mb-service-3</title> \
   </head> \
-  <body> \
+  <body style="background-color:CornflowerBlue;"> \
       <h1>Hello, you have reached mb-service-3!</h1> \
       <h1>Version: V1</h1>  \
   </body> \
